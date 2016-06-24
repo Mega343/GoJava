3b1d2957a8e8fc90215dfb4746844a0d690e0db7
артур <mega9594@Rambler.ru>
2016-06-24 21:32:59 +0300

    add module 3.2

--- src/test/java/EEModule3point2/SquareSumImplTest.java
+++ src/test/java/EEModule3point2/SquareSumImplTest.java
@@ -20,4 +20,13 @@ public class SquareSumImplTest {
         long expectedResult = 55;
         Assert.assertEquals(expectedResult, result);
     }
+
+    @Test
+    public void testGetSquareSumWithEvenElements() throws Exception {
+        int[] mas = {1,2,3,4};
+        int numberOfThreads = 3;
+        long result = example.getSquareSum(mas, numberOfThreads);
+        long expectedResult = 30;
+        Assert.assertEquals(expectedResult, result);
+    }
 }
